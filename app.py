fba46a18d3d4efd4b339c7f35a07c13132864396
Juan Pablo De la Cruz Rangel <A00843673@tec.mx>
2026-05-09 13:51:42 -0600

    Added general and code simple agents

--- app.py
+++ app.py
@@ -26,13 +26,50 @@ def chatbot():
 def chat():
     data = request.json
     prompt = data.get('prompt')
+    review_prompt = f"""
+Categorize the following user prompt into one of the following two categories: 
+'CODE' or 'DOUBT'. If the user wants to chat, solve a doubt or for any general 
+porpouse situation, classify it as 'DOUBT', if it wants to control a robot or 
+generate code, classify it as 'CODE'. Also, DO NOT MENTION IN YOUR ANY PART OF 
+YOUR ANSWER THE WORD 'DOUBT' OR 'CODE' IF YOU DO NOT CLASSIFY IT AS THAT, BECAUSE 
+IT CAN CONFUSE ME. 
+
+User prompt: {prompt}
+"""
+    category_answer = client.models.generate_content(
+        model = 'gemini-2.0-flash',
+        contents=review_prompt
+    )
+
+    category = category_answer.text.strip().upper()
+
+    if 'CODE' in category:
+        choosen_model = 'gemini-2.0-pro'
+        final_prompt = f"""You are a coding expert in manufacturing lines
+        and automation. Also, you are expert in webots simulator. Please
+        help me with the following:
+        
+        {prompt}
+FOR YOUR ANSWER FORMAT, ONLY PROVIDE THE PYTHON CODE OF THE WEBOTS CONTROLLER
+IN TXT FORMAT, FOLLOWING PYTHON SINTAXES AND STRUCTURE, SO THAT I CAN CHANGE
+THE EXTENTION FROM .TXT TO .PY, PUT IT INTO TE APPROPIATE FOLDER, AND WEBOTS
+WILL BE ABLE TO USE IT TO CONTROL THE ROBOT"""
+
+    elif 'DOUBT' in category:
+        choosen_model = 'gemini-2.0-flash'
+        final_prompt = f"""You are a teacher, expert in manufacturing lines
+        and automation. Also, you are expert in webots simulator. Please help
+        a student with the following question:
+
+        {prompt}"""
 
     try:
         response = client.models.generate_content(
-            model ='gemini-2.0-flash',
-            contents=prompt
+            model = choosen_model,
+            contents=final_prompt
         )
-        return jsonify({'reply': response.text})
+        return jsonify({'reply': response.text,
+                        'model': choosen_model})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
--- static/script.js
+++ static/script.js
@@ -1,7 +1,19 @@
+function downloadFile(content, filename) {
+    const data = new Blob([content], {type: 'text/plain'});
+    const url = window.URL.createObjectURL(data);
+    const link = document.createElement('a');
+
+    link.href = url;
+    link.download = filename;
+    link.click();
+    window.URL.revokeObjectURL(url);
+}
+
 const chatbot_btn = document.getElementById('chatbot_btn');
 const send_btn = document.getElementById('send_button');
 const answer = document.getElementById('gemini_answer');
 const servidor_btn = document.getElementById('simulator_btn');
+const download_py = document.getElementById('download_py');
 
 chatbot_btn.addEventListener('click', () => {
     window.location.href = '/chatbot';
@@ -11,27 +23,41 @@ servidor_btn.addEventListener('click', () => {
     window.location.href = '/simulator';
 });
 
-send_btn.addEventListener('click', async () => {
-    const prompt = document.getElementById('prompt_input').value;
+if (send_btn) {
+    send_btn.addEventListener('click', async () => {
+        answer.style.display = 'inline-block'
+        answer.innerText = 'Thinking...'
 
-    const response = await fetch('/api/chatbot', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({prompt: prompt})
+        const prompt = document.getElementById('prompt_input').value;
+
+        const response = await fetch('/api/chatbot', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({prompt: prompt})
+        });
+
+        const data = await response.json();
+
+        if (data.reply){
+            answer.innerText = data.reply;
+        }
+        else {
+            answer.innerText = data.error;
+        }
+
+        if (data.model === 'gemini-2.0-pro') {
+            download_py.style.display = 'inline-block'
+            download_py.onclick = () => downloadFile(data.reply, "controller.py")
+        }
+        else {
+            download_py.style.display = 'none'
+        }
     });
+}
 
-    const data = await response.json();
-
-    if (data.reply){
-        answer.innerText = data.reply;
-    }
-    else {
-        answer.innerText = data.error;
-    }
-});
 
 if (answer) {
-    answer.innerText = 'Thinking...';
+    answer.style.display = 'none';
 }
